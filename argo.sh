0b07d3f40f6442ad00270a8450d3d8eafed5d2e1
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2022-09-30 21:37:17 -0400

    formatting and color

--- argo.sh
+++ argo.sh
@@ -1,15 +1,16 @@
-echo Apply Argo CD
-kubectl create namespace argocd &&
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo -e \e[1;42mApply Argo CD \e[0m"
+kubectl create namespace argocd
+&& kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo Download Argo CD CLI
-sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64 &&
-sudo chmod +x /usr/local/bin/argocd
+echo -e \e[1;42mDownload Argo CD CLI \e[0m"
+sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+&& sudo chmod +x /usr/local/bin/argocd
 
-echo Access The Argo CD API Server
+echo -e \e[1;42mAccess The Argo CD API Server \e[0m"
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'        
 
 cat << EOF
+
 # port forward
 echo kubectl port-forward svc/argocd-server -n argocd 8080:443
 
