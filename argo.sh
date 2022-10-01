514b3523b6b2e18a3a950ba79c48089c8360b336
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2022-09-30 21:20:55 -0400

    move manual steps to playbook

--- argo.sh
+++ argo.sh
@@ -8,15 +8,3 @@ sudo chmod +x /usr/local/bin/argocd
 
 echo Access The Argo CD API Server
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'        
-
-cat << EOF
-# port forward
-echo kubectl port-forward svc/argocd-server -n argocd 8080:443
-# Login Using The CLI
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-argocd login localhost:8080  
-# change password
-argocd account update-password 
-# Register A Cluster To Deploy Apps To (Optional)
-kubectl config get-contexts -o name | argocd cluster add
-EOF
