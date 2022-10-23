a46b5f92271fea2a87e9fc4d809d902af032ce60
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2022-10-23 19:56:02 -0400

    combine login steps

--- argo.sh
+++ argo.sh
@@ -16,7 +16,7 @@ kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 # Login Using The CLI
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-argocd login localhost:8080  
+&& argocd login localhost:8080  
 
 # change password
 argocd account update-password 
