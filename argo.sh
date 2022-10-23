7f4bf23a54c6f5b3a8a5d005b7eedf5f31f3eb0a
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2022-10-23 19:56:41 -0400

    line continue

--- argo.sh
+++ argo.sh
@@ -15,7 +15,7 @@ cat << EOF
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 # Login Using The CLI
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo \
 && argocd login localhost:8080  
 
 # change password
